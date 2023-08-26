49b76435831f07075ec9590b8d4e499593e923a1
Weskley Bezerra <weskleymb@hotmail.com>
2023-08-26 11:29:09 -0300

    aula orientacao a objetos

--- conta.py
+++ conta.py
@@ -1,16 +1,13 @@
-class Conta:
+from abc import ABC, abstractmethod
 
-    # atributos
-    __agencia: int
-    __numero: int
-    __titular: str
-    __saldo = 0.0
+class Conta(ABC):
 
-    # construtor
+    # atributos e construtor
     def __init__(self, agencia: int, numero: int, titular: str):
         self.__agencia = agencia
         self.__numero = numero
         self.__titular = titular
+        self._saldo = 0.0
     
     # propriedades
     @property
@@ -31,25 +28,53 @@ class Conta:
 
     @property
     def saldo(self) -> float:
-        return self.__saldo
+        return self._saldo
     
     # metodos
     def depositar(self, valor: float):
         pode_depositar = valor > 0
         if pode_depositar:
-            self.__saldo += valor
+            self._saldo += valor
 
+    @abstractmethod
     def sacar(self, valor: float):
-        pode_sacar = valor > 0 and self.__saldo >= valor
-        if pode_sacar:
-            self.__saldo -= valor
+        pass
 
     # criar o metodo de transferir
     def transferir(self, valor: float, conta_destino):
-        pode_transferir = valor > 0 and self.__saldo >= valor
+        pode_transferir = valor > 0 and self._saldo >= valor
         if pode_transferir:
             self.sacar(valor)
             conta_destino.depositar(valor)
 
+    def __str__(self) -> str:
+        return f"Saldo de {self.titular} é {self.saldo}"
+
 class ContaPoupanca(Conta):
-    pass
+
+    __rendimento = 0.01
+
+    def render(self):
+        valor_rendimento = self.saldo * self.__rendimento
+        self.depositar(valor_rendimento)
+
+    def sacar(self, valor: float):
+        pode_sacar = valor > 0 and self._saldo >= valor
+        if pode_sacar:
+            self._saldo -= valor
+
+class ContaCorrente(Conta):
+    
+    __taxa = 0.05
+
+    def __init__(self, agencia: int, numero: int, titular: str, limite: float = 0.0):
+        self.__limite = limite
+        super().__init__(agencia, numero, titular)
+
+    def sacar(self, valor: float):
+        valor_da_taxa = valor * self.__taxa
+        valor_a_sacar = valor + valor_da_taxa
+        saldo_com_limite = self.saldo + self.__limite
+        pode_sacar = valor > 0 and saldo_com_limite >= valor_a_sacar
+        if pode_sacar:
+            self._saldo -= valor_a_sacar
--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from lampada import Lampada
-from conta import Conta, ContaPoupanca
+from conta import ContaCorrente, ContaPoupanca
 
-conta_pedro = Conta(123, 147, "Pedro")
-conta_pedro.depositar(1000)
-print(f"Saldo de {conta_pedro.titular} é {conta_pedro.saldo}")
-
-conta_jose = Conta(456, 147, "José")
+conta_jose = ContaCorrente(456, 147, "José", 1000)
 conta_jose.depositar(1000)
-print(f"Saldo de {conta_jose.titular} é {conta_jose.saldo}")
-
-conta_pedro.transferir(250, conta_jose)
-
-print(f"Saldo de {conta_pedro.titular} é {conta_pedro.saldo}")
-print(f"Saldo de {conta_jose.titular} é {conta_jose.saldo}")
+print(conta_jose)
 
 conta_daniele = ContaPoupanca(963, 854, "Daniele")
+conta_daniele.depositar(1000)
+print(conta_daniele)
+
+conta_jose.sacar(1500)
+
+print(conta_jose)
